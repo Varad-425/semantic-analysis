1143e01ce4740d25e130b3ad483882992ec7e1df
Varad-425 <varad.4250@gmail.com>
2023-05-15 16:04:33 +0530

    Adding comments

--- lhp.py
+++ lhp.py
@@ -14,14 +14,13 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-# read data
 
 
 class type_Tweet():
     def __init__(self):
         nltk.download('stopwords')
         self.__data = pd.read_csv(
-            'F:\CODING_AFTER_AUGUST\ML_Projects\sentiment analysis\\twitter_training.csv')
+            'F:\CODING_AFTER_AUGUST\ML_Projects\sentiment_analysis\\twitter_training.csv')
         self.__cv = CountVectorizer(max_features=1500)
         self.__lr = LabelEncoder()
         self.__ps = PorterStemmer()
@@ -32,11 +31,12 @@ class type_Tweet():
         self.new_set = set(self.__data.iloc[:, 1])
         df = pd.DataFrame(self.new_set, columns=[
                           'Category'], index=range(1, len(self.new_set)+1))
-        print(df)
 
+    # CountVectorize here
     def get_counts(self, data):
         return self.__cv.transform(data).toarray()  # type: ignore
 
+    # Removing stopword from the data
     def removing_Stopwords(self, data):
         review = re.sub('[^a-zA-Z]', ' ', str(data))
         review = review.lower()
@@ -47,7 +47,9 @@ class type_Tweet():
         self.__corpus.append(review)
         return review
 
+    # Getting the categorical data
     def get_data(self, category):
+        # 'Borderlands' is the column name for category column
         xds = self.__data[self.__data['Borderlands'] == category]
         X_ds = xds.iloc[:, 3].values
         Y_ds = xds.iloc[:, 2].values
@@ -57,14 +59,17 @@ class type_Tweet():
         self.__cv.fit(self.__corpus)
         self.__X = self.get_counts(self.__corpus)
 
+    # splitting dataset
     def split_data(self):
         self.__X_train, self.__X_test, self.__Y_train, self.__Y_test = train_test_split(
             self.__X, self.__Y, test_size=0.2, random_state=0)
 
+    # Logistic Classification model
     def logistic_reg(self):
         self.__gb = LogisticRegression(random_state=0, max_iter=2000)
         self.__gb.fit(self.__X_train, self.__Y_train)
 
+    # Prediction method by entering the tweet and return the result
     def prediction(self, data):
         print("*"*50)
         print('\n\n'+data[0]+'\n\n')
@@ -86,13 +91,16 @@ class type_Tweet():
         print("*"*50)
         return pred
 
+    # Accuracy score of model dataset
     def get_accuracy(self):
         return accuracy_score(self.__Y_test, self.__gb.predict(self.__X_test))
 
+    # Confusion matrix of model dataset
     def get_confusion_matrix(self):
         cm = confusion_matrix(self.__Y_test, self.__gb.predict(self.__X_test))
         return cm
 
+    # Standard scaling of the dataset
     def standard_scale(self, data=None):
         if data is None:
             self.__X_train = self.__sc.fit_transform(self.__X_train)
@@ -101,6 +109,7 @@ class type_Tweet():
             data = self.__sc.transform(data)
             return data
 
+    # this method is called where you enter a string category and will apply the model and then use prediction
     def run(self, category):
         self.get_data(category)
         self.split_data()
@@ -109,18 +118,23 @@ class type_Tweet():
         self.__categoory = category
 
 
+# Creating object class
 obj = type_Tweet()
 print("*"*50, "\n\n")
 while (True):
+    # Enter the category from the list you get while running
     catg = str(input("Enter the category : "))
     if catg in obj.new_set:
         break
     else:
         print("Wrong input., Try again")
 
+
 inputs = input("Enter the tweet : ")
-inputs = ["im like the game and play for like 5 hours and its amazing"]
+# Primary use this method for setting up the model
 obj.run(catg)
+
+# Use for prediction of the tweet
 pred = obj.prediction(inputs)
 print(obj.get_accuracy())
 print(obj.get_confusion_matrix())
